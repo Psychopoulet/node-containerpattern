566151d21b088999d21b169ca570b0295cf59785
Seb - physical <svidal1@free.fr>
2022-05-19 18:12:08 +0200

    improve typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,10 @@
+/// <reference types="node" />
+
 declare module "node-containerpattern" {
 
 	type tType = "array" | "boolean" | "color" | "email" | "float" | "ipv4" | "ipv6" | "integer" | "object" | "string" | "url";
 
-	class Container {
+	class Container extends Map {
 
 		// Map
 		public size: number;
@@ -17,34 +19,24 @@ declare module "node-containerpattern" {
 
 		constructor(recursionSeparator?: string);
 
-		// Map
-		public keys(): Iterator<string>;
-		public values(): Iterator<any>;
-		public forEach(callback: (key: string, value: any) => void): void;
-
 		protected _ensureData(key: string, value: any): any;
 		protected _ensureDataRecursive(key: string, value: any): any;
 		protected _createBaseObject(parentKey: string, parentValue: object, keys: Array<string>, i: number, value: any): object;
 		protected _extractDocumentation(previousKeys: string, object: object): object;
 
-		public clear(): Container;
-		public clearData(): Container;
-		public clearDocumentations(): Container;
-		public clearLimits(): Container;
-		public clearMinsMaxs(): Container;
-		public clearRegexs(): Container;
-		public clearSkeletons(): Container;
-		public delete(key: string): Container;
-		public document(key: string, value: string): Container;
+		public clearData(): this;
+		public clearDocumentations(): this;
+		public clearLimits(): this;
+		public clearMinsMaxs(): this;
+		public clearRegexs(): this;
+		public clearSkeletons(): this;
+		public document(key: string, value: string): this;
 		public documentation(): object;
-		public get(key: string): any;
-		public has(key: string): boolean;
-		public limit(key: string, limit: Array<any>): Container;
-		public min(key: string, min: number): Container;
-		public max(key: string, max: number): Container;
-		public regex(key: string, regex: RegExp): Container;
-		public set(key: string, value: any): Container;
-		public skeleton(key: string, value: tType): Container;
+		public limit(key: string, limit: Array<any>): this;
+		public min(key: string, min: number): this;
+		public max(key: string, max: number): this;
+		public regex(key: string, regex: RegExp): this;
+		public skeleton(key: string, value: tType): this;
 
 	}
 
