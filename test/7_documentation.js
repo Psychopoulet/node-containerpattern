e3f985452ebf1da82ec6ba824c1cbdb7cb10f987
Seb - physical <svidal1@free.fr>
2022-05-24 18:10:40 +0200

    improve documentation export & typings

--- test/7_documentation.js
+++ test/7_documentation.js
@@ -40,13 +40,9 @@ describe("documentation", () => {
 			.set("teststring", "test 1");
 
 		deepStrictEqual(container.documentation().teststring, {
-			"documentation": "",
 			"fullkey": "teststring",
-			"limits": [ "test 1", "test 2" ],
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "string",
+			"limits": [ "test 1", "test 2" ],
 			"value": "test 1"
 		}, "normal running has invalid return for \"limits\"");
 
@@ -68,82 +64,43 @@ describe("documentation", () => {
 
 		deepStrictEqual(container.documentation(), {
 			"teststring": {
-				"documentation": "",
 				"fullkey": "teststring",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "string",
 				"value": "string"
 			},
 			"testboolean": {
-				"documentation": "",
 				"fullkey": "testboolean",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "boolean",
 				"value": false
 			},
 			"testinteger": {
-				"documentation": "",
 				"fullkey": "testinteger",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "integer",
 				"value": 1
 			},
 			"testbase16": {
-				"documentation": "",
 				"fullkey": "testbase16",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "integer",
 				"value": 165
 			},
 			"testfloat": {
-				"documentation": "",
 				"fullkey": "testfloat",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "float",
 				"value": 1.1
 			},
 			"testemptyarray": {
-				"documentation": "",
 				"fullkey": "testemptyarray",
-				"limits": null,
-				"min": 0,
-				"max": null,
-				"regex": null,
 				"type": "array",
+				"min": 0,
 				"content": {}
 			},
 			"testemptyobject": {
-				"documentation": "",
 				"fullkey": "testemptyobject",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "object",
 				"content": {}
 			},
 			"testfunction": {
-				"documentation": "",
 				"fullkey": "testfunction",
-				"limits": null,
-				"min": null,
-				"max": null,
-				"regex": null,
 				"type": "function"
 			}
 		}, "normal running has invalid return for \"not typed\"");
@@ -160,57 +117,37 @@ describe("documentation", () => {
 			.set("testfloat", 1.1).document("testfloat", "This is a float");
 
 		deepStrictEqual(container.documentation().teststring, {
-			"documentation": "This is a string",
 			"fullkey": "teststring",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "string",
+			"documentation": "This is a string",
 			"value": "string"
 		}, "normal running has invalid return for \"teststring\"");
 
 		deepStrictEqual(container.documentation().testboolean, {
-			"documentation": "This is a boolean",
 			"fullkey": "testboolean",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "boolean",
+			"documentation": "This is a boolean",
 			"value": false
 		}, "normal running has invalid return for \"testboolean\"");
 
 		deepStrictEqual(container.documentation().testinteger, {
-			"documentation": "This is an integer",
 			"fullkey": "testinteger",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "integer",
+			"documentation": "This is an integer",
 			"value": 1
 		}, "normal running has invalid return for \"testinteger\"");
 
 		deepStrictEqual(container.documentation().testbase16, {
-			"documentation": "This is a base16",
 			"fullkey": "testbase16",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "integer",
+			"documentation": "This is a base16",
 			"value": 165
 		}, "normal running has invalid return for \"testbase16\"");
 
 		deepStrictEqual(container.documentation().testfloat, {
-			"documentation": "This is a float",
 			"fullkey": "testfloat",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "float",
+			"documentation": "This is a float",
 			"value": 1.1
 		}, "normal running has invalid return for \"testfloat\"");
 
@@ -239,52 +176,46 @@ describe("documentation", () => {
 		// ipv4
 
 		deepStrictEqual(container.documentation().testipv4, {
-			"documentation": "This is an ipv4",
 			"fullkey": "testipv4",
-			"limits": null,
+			"type": "ipv4",
+			"documentation": "This is an ipv4",
 			"min": 7,
 			"max": 15,
 			"regex": stringifyRegex(patternIPV4),
-			"type": "ipv4",
 			"value": "127.0.0.1"
 		}, "normal running has invalid return for \"testipv4\"");
 
 		// ipv6
 
 		deepStrictEqual(container.documentation().testipv6, {
-			"documentation": "This is an ipv6",
 			"fullkey": "testipv6",
-			"limits": null,
+			"type": "ipv6",
+			"documentation": "This is an ipv6",
 			"min": 7,
 			"max": 39,
 			"regex": stringifyRegex(patternIPV6),
-			"type": "ipv6",
 			"value": "0000:0000:0000:0000:0000:0000:0000:0001"
 		}, "normal running has invalid return for \"testipv6\"");
 
 		// email
 
 		deepStrictEqual(container.documentation().testemail, {
-			"documentation": "This is an email",
 			"fullkey": "testemail",
-			"limits": null,
-			"min": 6,
-			"max": null,
-			"regex": stringifyRegex(patternEmail),
 			"type": "email",
+			"documentation": "This is an email",
+			"min": 6,
+			"regex": stringifyRegex(patternEmail),
 			"value": "test@test.com"
 		}, "normal running has invalid return for \"testemail\"");
 
 		// url
 
 		deepStrictEqual(container.documentation().testurl, {
-			"documentation": "This is an url",
 			"fullkey": "testurl",
-			"limits": null,
-			"min": 8,
-			"max": null,
-			"regex": stringifyRegex(patternUrl),
 			"type": "url",
+			"documentation": "This is an url",
+			"min": 8,
+			"regex": stringifyRegex(patternUrl),
 			"value": "https://www.google.com"
 		}, "normal running has invalid return for \"testurl\"");
 
@@ -303,42 +234,26 @@ describe("documentation", () => {
 				.set("testnotemptyarray", [ "test", "test" ]);
 
 		deepStrictEqual(container.documentation().testemptyarray, {
-			"documentation": "This is an empty array",
 			"fullkey": "testemptyarray",
-			"limits": null,
-			"min": 0,
-			"max": null,
-			"regex": null,
 			"type": "array",
+			"documentation": "This is an empty array",
+			"min": 0,
 			"content": {}
 		}, "normal running has invalid return for \"testemptyarray\"");
 
 		deepStrictEqual(container.documentation().testnotemptyarray, {
-			"documentation": "This is a not empty array",
 			"fullkey": "testnotemptyarray",
-			"limits": null,
-			"min": 0,
-			"max": null,
-			"regex": null,
 			"type": "array",
+			"documentation": "This is a not empty array",
+			"min": 0,
 			"content": {
 				"0": {
-					"documentation": "",
 					"fullkey": "testnotemptyarray.0",
-					"limits": null,
-					"min": null,
-					"max": null,
-					"regex": null,
 					"type": "string",
 					"value": "test"
 				},
 				"1": {
-					"documentation": "",
 					"fullkey": "testnotemptyarray.1",
-					"limits": null,
-					"min": null,
-					"max": null,
-					"regex": null,
 					"type": "string",
 					"value": "test"
 				}
@@ -371,73 +286,41 @@ describe("documentation", () => {
 		// object
 
 		deepStrictEqual(container.documentation().testemptyobject, {
-			"documentation": "This is an empty object",
 			"fullkey": "testemptyobject",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
+			"documentation": "This is an empty object",
 			"content": {}
 		}, "normal running has invalid return for \"testemptyobject\"");
 
 		deepStrictEqual(container.documentation().testnotinstanciedobject, {
-			"documentation": "",
 			"fullkey": "testnotinstanciedobject",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "function"
 		}, "normal running has invalid return for \"testnotinstanciedobject\"");
 
 		deepStrictEqual(container.documentation().testnotemptyobject, {
-			"documentation": "This is a not empty object",
 			"fullkey": "testnotemptyobject",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
+			"documentation": "This is a not empty object",
 			"content": {
 				"array": {
-					"documentation": "",
 					"fullkey": "testnotemptyobject.array",
-					"limits": null,
-					"max": null,
 					"min": 0,
-					"regex": null,
 					"type": "array",
 					"content": {
 						"0": {
-							"documentation": "",
 							"fullkey": "testnotemptyobject.array.0",
-							"limits": null,
-							"max": null,
-							"min": null,
-							"regex": null,
 							"type": "string",
 							"value": "test1"
 						},
 						"1": {
-							"documentation": "",
 							"fullkey": "testnotemptyobject.array.1",
-							"limits": null,
-							"max": null,
-							"min": null,
-							"regex": null,
 							"type": "string",
 							"value": "test2"
 						}
 					}
 				},
 				"test": {
-					"documentation": "",
 					"fullkey": "testnotemptyobject.test",
-					"limits": null,
-					"max": null,
-					"min": null,
-					"regex": null,
 					"type": "string",
 					"value": "test"
 				}
@@ -454,21 +337,12 @@ describe("documentation", () => {
 				.set("module.versions", []);
 
 		deepStrictEqual(container.documentation().module, {
-			"documentation": "",
 			"fullkey": "module",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
 			"content": {
 				"versions": {
-					"documentation": "",
 					"fullkey": "module.versions",
-					"limits": null,
 					"min": 0,
-					"max": null,
-					"regex": null,
 					"type": "array",
 					"content": {}
 				}
@@ -487,30 +361,17 @@ describe("documentation", () => {
 					.set("module.versions.0", 1);
 
 		deepStrictEqual(container.documentation().module, {
-			"documentation": "",
 			"fullkey": "module",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
 			"content": {
 				"versions": {
-					"documentation": "",
 					"fullkey": "module.versions",
-					"limits": null,
-					"min": 0,
-					"max": null,
-					"regex": null,
 					"type": "array",
+					"min": 0,
 					"content": {
 						"0": {
 							"documentation": "This is the first version",
 							"fullkey": "module.versions.0",
-							"limits": null,
-							"min": null,
-							"max": null,
-							"regex": null,
 							"type": "integer",
 							"value": 1
 						}
@@ -529,21 +390,11 @@ describe("documentation", () => {
 				.set("module.version", {});
 
 		deepStrictEqual(container.documentation().module, {
-			"documentation": "",
 			"fullkey": "module",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
 			"content": {
 				"version": {
-					"documentation": "",
 					"fullkey": "module.version",
-					"limits": null,
-					"min": null,
-					"max": null,
-					"regex": null,
 					"type": "object",
 					"content": {}
 				}
@@ -567,40 +418,24 @@ describe("documentation", () => {
 					.set("module.version.code", "0.0.1");
 
 		deepStrictEqual(container.documentation().module, {
-			"documentation": "",
 			"fullkey": "module",
-			"limits": null,
-			"min": null,
-			"max": null,
-			"regex": null,
 			"type": "object",
 			"content": {
 				"version": {
-					"documentation": "",
 					"fullkey": "module.version",
-					"limits": null,
-					"min": null,
-					"max": null,
-					"regex": null,
 					"type": "object",
 					"content": {
 						"name": {
 							"documentation": "Version name",
 							"fullkey": "module.version.name",
-							"limits": null,
 							"min": 0,
-							"max": null,
-							"regex": null,
 							"type": "string",
 							"value": "main"
 						},
 						"code": {
 							"documentation": "Version code",
 							"fullkey": "module.version.code",
-							"limits": null,
 							"min": 0,
-							"max": null,
-							"regex": null,
 							"type": "string",
 							"value": "0.0.1"
 						}
