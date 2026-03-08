47a9ffef4fac7f3e3b4361c5f72e93216d03917d
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-03-08 20:13:13 +0100

    standardize & modernize

--- .eslintrc-src.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "extends": [ "plugin:eslint-plugin-personnallinter/ts-back" ],
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unsafe-argument": "warn",
-        "@typescript-eslint/no-unsafe-assignment": "warn"
-    }
-};
